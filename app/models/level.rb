e82e1d6738a6baee59f488a5afb8c2e484215aa9
w0258129 <w0258129@nscc.ca>
2017-03-03 13:30:01 -0400

    Add information into Years and Levels models.

--- app/models/level.rb
+++ app/models/level.rb
@@ -1,2 +1,6 @@
 class Level < ApplicationRecord
+  self.table_name = 'Level'
+  self.primary_key = :id
+
+  belongs_to :book, :class_name => 'Book', :foreign_key => :LevelId
 end
--- app/models/year.rb
+++ app/models/year.rb
@@ -1,2 +1,6 @@
 class Year < ApplicationRecord
+  self.table_name = 'Year'
+  self.primary_key = :id
+
+  belongs_to :book, :class_name => 'Book', :foreign_key => :YearId
 end
