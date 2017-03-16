c72a9728571bd149b7be1bb6ddcae94c82fa73ed
w0275891 <w0275891@nscc.ca>
2017-03-16 23:05:15 +0000

    adding devise user and admin model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
@@ -9,4 +11,6 @@ Rails.application.routes.draw do
   resources :roles
   resources :users
   resources :levels
+
+  root 'teams#index'
 end
